18cae053f74c986aeaf45b7b1b16daad492c9d2c
DarioFi <filatrella.dario@gmail.com>
2024-03-13 16:47:56 +0100

    chore: added script for converting pdf into mmd

---
+++ data_processing/run_predict_marker.sh
@@ -0,0 +1,2 @@
+source ../venv_marker/bin/activate
+python marker/convert_single.py "pre_processed_books/complex_analysis_2.pdf" "processed_books/marker" --parallel_factor 2 --max_pages 10
---
+++ data_processing/run_predict_nougat.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+nougat "pre_processed_books/" -o "processed_books/" --full-precision -p 1-10
