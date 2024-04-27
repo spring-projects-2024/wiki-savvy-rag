5bdf732046810e2b6dba973d7a19802c98b137ca
MattiaSC01 <mattia.scardecchia@studbocconi.it>
2024-04-27 18:12:10 +0200

    chore: organize clean_dump better

--- wikipedia_data/clean_dump.py
+++ wikipedia_data/clean_dump.py
@@ -2,7 +2,7 @@ from html import unescape
 from tqdm import tqdm
 from utils import scroll_pages
 import re
-from utils import remove_table_tags, remove_template_tags, remove_wiki_tags
+from utils import remove_table_tags, remove_template_tags, remove_wiki_tags, remove_square_brackets_around_links
 
 
 N_PAGES = 2357969
@@ -17,20 +17,23 @@ ref_tag_reg = r"<(ref|span|gallery|timeline|imagemap|mapframe|div).*?/(\1|)>"  #
 remove_ref_tag = re.compile(ref_tag_reg, re.DOTALL)
 
 
+def process_page(page):
+    page = unescape(page)
+    page = asterisk_regex.sub("", page)
+    page = remove_html_comment.sub("", page)
+    page = remove_ref_tag.sub("", page)
+
+    page = remove_table_tags(page)
+    page = remove_template_tags(page)
+    page = remove_wiki_tags(page)
+    page = remove_square_brackets_around_links(page)
+
+    page = "\n".join([s for s in page.split("\n") if s])  # remove empty lines
+    return page + "\n"
+
+
 if __name__ == "__main__":
     with open(output_file, "w") as out:
         with open(input_file, "r") as f:
             for page in tqdm(scroll_pages(f), total=N_PAGES):
-
-                page = unescape(page)
-                page = asterisk_regex.sub("", page)
-                page = remove_html_comment.sub("", page)
-                page = remove_ref_tag.sub("", page)
-
-                page = remove_table_tags(page)
-                page = remove_template_tags(page)
-                page = remove_wiki_tags(page)
-
-                page = "\n".join([s for s in page.split("\n") if s])  # remove empty lines
-                out.write(page)
-                out.write("\n")  # ensure newline at end of page
+                out.write(process_page(page))
