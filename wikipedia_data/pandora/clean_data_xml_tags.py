378c22207aea482a82684291bfdd1f97693ace50
MattiaSC01 <mattia.scardecchia@studbocconi.it>
2024-04-27 16:15:25 +0200

    chore: regorganize explore_category_tree

--- wikipedia_data/pandora/clean_data_xml_tags.py
+++ wikipedia_data/pandora/clean_data_xml_tags.py
@@ -36,7 +36,8 @@ with open("subsample_out_4_no_xml.xml", "a") as error_f:
                     new_page = etree.tostring(tree, encoding="unicode")
                     new_page = new_page.replace("* \n", "")
                     out.write(new_page)
-                except:
+                except Exception as e:
+                    print(e)
                     c_err += 1
                     new_page = p
                     error_f.write(new_page)
