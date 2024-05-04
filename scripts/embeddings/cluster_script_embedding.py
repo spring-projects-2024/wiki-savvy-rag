0074515d2d638a7b176a315d2f0aff811a3be6c5
FedeZara <fedezara2001@gmail.com>
2024-05-04 19:42:53 +0200

    fix: fixed embedding script for cluster

--- scripts/embeddings/cluster_script_embedding.py
+++ scripts/embeddings/cluster_script_embedding.py
@@ -1,25 +1,32 @@
-import json
 from typing import List
 
 import torch
 from tqdm import tqdm
 
-from embeddings import EmbedderWrapper
+from backend.vector_database.embedder_wrapper import EmbedderWrapper
 from backend.data_cleaning import utils
 
-input_file = "../wikipedia_data/subsample_chunkeder.xml"
-output_file = "../wikipedia_data/embeddings"
+INPUT_FILE = "wikidump_processing/data/subsample_chunkeder.xml"
+OUTPUT_FILE = "scripts/embeddings/data/embeddings"
+PAGES_TO_PROCESS = 2
+MAX_ACCUMULATION = 1
+
+
+def dump_embeddings(acc_embeds_list, filename):
+    stacked_embeddings = torch.cat(acc_embeds_list, dim=0)
+
+    torch.save(stacked_embeddings, filename)
+
 
 if __name__ == "__main__":
 
     embedder = EmbedderWrapper("cpu")
-
-    MAX_ACCUMULATION = 300_000
     file_counter = 0
     acc_embeds_list: List = []
     running_sum = 0
 
-    with open(input_file, "r") as f:
+    with open(INPUT_FILE, "r") as f:
+        count = 0
         for raw_page in tqdm(utils.scroll_pages(f), total=utils.N_PAGES):
 
             page = utils.get_extracted_page_chunks(raw_page)
@@ -31,10 +38,17 @@ if __name__ == "__main__":
             running_sum += embeddings.shape[0]
 
             if running_sum > MAX_ACCUMULATION:
-
-                stacked_embeddings = torch.cat(acc_embeds_list, dim=0)
-
-                torch.save(stacked_embeddings, output_file + f"{file_counter}.pt")
+                dump_embeddings(
+                    acc_embeds_list, filename=OUTPUT_FILE + f"{file_counter}.pt"
+                )
                 file_counter += 1
                 running_sum = 0
                 acc_embeds_list = []
+
+            count += 1
+
+            if count >= PAGES_TO_PROCESS:
+                break
+
+    if len(acc_embeds_list) > 0:
+        dump_embeddings(acc_embeds_list, filename=OUTPUT_FILE + f"{file_counter}.pt")
