a1b94c647e1b6dde157e1eee60226d64d0b9dd5a
MattiaSC01 <mattia.scardecchia@studbocconi.it>
2024-05-04 17:43:54 +0200

    feat: tentative mmlu script

--- backend/model/rag_handler.py
+++ backend/model/rag_handler.py
@@ -1,3 +1,4 @@
+from copy import deepcopy
 from typing import Optional
 from backend.model.llm_handler import LLMHandler
 from backend.vector_database.faiss_wrapper import FaissWrapper
@@ -12,6 +13,7 @@ class RagHandler:
             self, 
             model_name: str, 
             device: str, 
+            rag_config: Optional[dict] = None,
             model_kwargs: Optional[dict] = None, 
             tokenizer_kwargs: Optional[dict] = None, 
             faiss_kwargs: Optional[dict] = None,
@@ -19,6 +21,9 @@ class RagHandler:
         for kwargs in (model_kwargs, tokenizer_kwargs, faiss_kwargs):
             if kwargs is None:
                 kwargs = {}
+        if rag_config is None:
+            rag_config = self.get_default_rag_config()
+        self.rag_config = rag_config
         self.faiss = FaissWrapper(device=device, **faiss_kwargs)
         self.llm = LLMHandler(
             device=device, 
@@ -27,16 +32,27 @@ class RagHandler:
             tokenizer_kwargs=tokenizer_kwargs
         )
 
-    def inference(self, history, query, use_rag=True, **kwargs) -> str:
+    def get_default_rag_config(self):
+        # TODO: choose default values
+        return {
+            "max_new_tokens": 500,
+            "return_full_text": False,
+            "temperature": 0.0,
+            "do_sample": False,
+        }
 
+    def inference(self, history, query, use_rag=True, **kwargs) -> str:
         if use_rag is False:
             messages = join_messages_query_no_rag(history, query)
         else:
             retrieved = self.faiss.search_text(query)
             # here we would do some preprocessing on the retrieved documents
             messages = join_messages_query_rag(history, query, retrieved)
-
-        response = self.llm.inference(messages, kwargs)
+        
+        rag_config = deepcopy(self.rag_config)
+        if kwargs:
+            rag_config.update(kwargs)
+        response = self.llm.inference(messages, rag_config)
         return response
 
     def add_arxiv_paper(self, paper):
---
+++ scripts/benchmark/mmlu.py
@@ -0,0 +1,70 @@
+import json
+from backend.benchmark.utils import format_question, craft_query, load_mmlu
+import argparse
+import yaml
+import datasets
+from backend.model.rag_handler import RagHandler
+
+
+def evaluate(rag_handler: RagHandler, dataset: datasets.Dataset, k_shot: int = 0):
+    metrics = {}
+    examples = [dataset[i] for i in range(k_shot)]  # k-shot evaluation
+
+    for question in dataset:
+        query = craft_query(question, chat=True, examples=examples)
+        response = rag_handler.inference([], query)
+        response = response[0]["generated_text"]
+        response = response.split("\n")[-1].strip()
+        if response == chr(65 + question["answer"]):
+            metrics["correct"] += 1
+        metrics["total"] += 1
+
+    metrics["accuracy"] = metrics["correct"] / metrics["total"]
+    return metrics
+
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--split", type=str, default="test")
+    parser.add_argument("--subset", type=str, default="stem")
+    parser.add_argument("--output", type=str, default="mmlu.json")
+    parser.add_argument("--config_path", type=str, default="configs/llm.yaml")
+    args = parser.parse_args()
+
+    with open(args.config_path, "r") as f:
+        config = yaml.safe_load(f)
+
+    dataset = load_mmlu(split=args.split, subset=args.subset)
+    print(f"Loaded {len(dataset)} questions from the {args.subset} subset of the {args.split} split of the MMLU dataset.")
+
+    device = config["device"]
+    model_name = config["model_name"]
+    model_kwargs = config.get("model_kwargs", None)
+    tokenizer_kwargs = config.get("tokenizer_kwargs", None)
+    faiss_kwargs = config.get("faiss_kwargs", None)
+
+    rag_kwargs = config.get("rag_kwargs", {})
+    rag_kwargs["max_new_tokens"] = 1  # this overrides the config file. We only need the first token.
+
+    print("Creating RAGHandler...")
+    rag_handler = RagHandler(
+        model_name=model_name,
+        device=device,
+        rag_config=rag_kwargs,
+        model_kwargs=model_kwargs,
+        tokenizer_kwargs=tokenizer_kwargs,
+        faiss_kwargs=faiss_kwargs,
+    )
+
+    print("Starting evaluation...")
+    metrics = evaluate(rag_handler, dataset)
+    print("Evaluation done.")
+
+    with open(args.output, "w") as f:
+        print(f"Saving metrics to {args.output}")
+        json.dump(metrics, f)
+
+
+if __name__ == "__main__":
+    main()
