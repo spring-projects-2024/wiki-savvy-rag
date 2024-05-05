b1666c2c68fa50e61ace9dda42173596e6527a22
FedeZara <fedezara2001@gmail.com>
2024-05-05 11:19:31 +0200

    feat: embedding script ready for cluster

--- scripts/embeddings/cluster_script_embedding.py
+++ scripts/embeddings/cluster_script_embedding.py
@@ -1,33 +1,69 @@
 from typing import List
 
+import os
 import torch
 from tqdm import tqdm
 
 from backend.vector_database.embedder_wrapper import EmbedderWrapper
 from backend.data_cleaning import utils
+import argparse
 
-INPUT_FILE = "wikidump_processing/data/subsample_chunkeder.xml"
-OUTPUT_FILE = "scripts/embeddings/data/embeddings"
-PAGES_TO_PROCESS = 2
-MAX_ACCUMULATION = 1
+
+INPUT_FILE_DEFAULT = "wikidump_processing/data/subsample_chunkeder.xml"
+OUTPUT_DIR_DEFAULT = "scripts/embeddings/data/"
+PAGES_TO_PROCESS_DEFAULT = utils.N_PAGES
+MAX_ACCUMULATION_DEFAULT = 300_000
 
 
 def dump_embeddings(acc_embeds_list, filename):
+    print("Dump embeddings file: ", filename)
     stacked_embeddings = torch.cat(acc_embeds_list, dim=0)
 
     torch.save(stacked_embeddings, filename)
 
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+        "--input",
+        type=str,
+        default=INPUT_FILE_DEFAULT,
+        help="Location of the subsample_chunkeder.xml file",
+    )
+    parser.add_argument(
+        "--output_dir",
+        type=str,
+        default=OUTPUT_DIR_DEFAULT,
+        help="Location of the directory where to store the output files",
+    )
+    parser.add_argument(
+        "--pages",
+        type=int,
+        default=PAGES_TO_PROCESS_DEFAULT,
+        help="Maximum amount of pages to process",
+    )
+    parser.add_argument(
+        "--max_accumulation",
+        type=int,
+        default=MAX_ACCUMULATION_DEFAULT,
+        help="Maximum number of chunks to keep on memory before dumping the embeddings on file",
+    )
+
+    args = parser.parse_args()
 
     embedder = EmbedderWrapper("cpu")
     file_counter = 0
     acc_embeds_list: List = []
     running_sum = 0
 
-    with open(INPUT_FILE, "r") as f:
+    if not os.path.exists(args.output_dir):
+        print("Specified directory doesn't exist. Creating it...")
+        os.mkdir(args.output_dir)
+
+    with open(args.input, "r") as f:
         count = 0
-        for raw_page in tqdm(utils.scroll_pages(f), total=utils.N_PAGES):
+        for raw_page in tqdm(utils.scroll_pages(f), total=args.pages):
 
             page = utils.get_extracted_page_chunks(raw_page)
 
@@ -37,9 +73,10 @@ if __name__ == "__main__":
             acc_embeds_list.append(embeddings)
             running_sum += embeddings.shape[0]
 
-            if running_sum > MAX_ACCUMULATION:
+            if running_sum > args.max_accumulation:
                 dump_embeddings(
-                    acc_embeds_list, filename=OUTPUT_FILE + f"{file_counter}.pt"
+                    acc_embeds_list,
+                    filename=args.output_dir + f"embeddings{file_counter}.pt",
                 )
                 file_counter += 1
                 running_sum = 0
@@ -47,8 +84,10 @@ if __name__ == "__main__":
 
             count += 1
 
-            if count >= PAGES_TO_PROCESS:
+            if count >= args.pages:
                 break
 
     if len(acc_embeds_list) > 0:
-        dump_embeddings(acc_embeds_list, filename=OUTPUT_FILE + f"{file_counter}.pt")
+        dump_embeddings(
+            acc_embeds_list, filename=args.output_dir + f"{file_counter}.pt"
+        )
---
+++ scripts/embeddings/cluster_script_embedding.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+python scripts/embeddings/cluster_script_embedding.py --input "wikidump_processing/data/subsample_chunkeder.xml" --output_dir "scripts/embeddings/data/" --max_accumulation 300000
