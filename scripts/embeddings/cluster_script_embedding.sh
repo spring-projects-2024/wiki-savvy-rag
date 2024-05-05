f3689ecbb3ccc0c0ff93edbfb93b2f07488cf110
FedeZara <fedezara2001@gmail.com>
2024-05-05 19:15:13 +0200

    chore: remove bash script

--- scripts/embeddings/cluster_script_embedding.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-python scripts/embeddings/cluster_script_embedding.py --device "cuda:1" --db_dir "scripts/dataset/data" --db_name="dataset" --output_dir "scripts/embeddings/data/" --max_accumulation 300000 
