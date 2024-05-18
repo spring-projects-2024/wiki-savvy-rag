e1cc19dd7ab9cfe6b6be707c74f989ac9c5d5f18
C4ntor <116038756+C4ntor@users.noreply.github.com>
2024-05-18 19:50:21 +0200

    feat: allowing markdown and laTeX together with live token generation

--- frontend/app.py
+++ frontend/app.py
@@ -5,12 +5,13 @@ import time
 
 st.title("Wikipedia Savvy")
 
+
 @st.cache_resource
 def build_rag_handler():
     return RagHandler(
         model_name="Minami-su/Qwen1.5-0.5B-Chat_llamafy",
-        #"microsoft/phi-3-mini-128k-instruct",
-        #"Minami-su/Qwen1.5-0.5B-Chat_llamafy"
+        # "microsoft/phi-3-mini-128k-instruct",
+        # "Minami-su/Qwen1.5-0.5B-Chat_llamafy"
         device="cpu",
         model_kwargs={
             "torch_dtype": "auto",
@@ -23,11 +24,13 @@ rag = build_rag_handler()
 
 
 if "messages" not in st.session_state:
-    st.session_state['messages'] = []
+    st.session_state["messages"] = []
 
 for message in st.session_state.messages:
     with st.chat_message(message["role"]):
-        st.markdown(message["content"])
+        st.markdown(
+            message["content"]
+        )  # allow to have latex and markdown code after generation
 
 if prompt := st.chat_input("What do you want to know?"):
     st.chat_message("user").markdown(prompt)
@@ -37,7 +40,7 @@ if prompt := st.chat_input("What do you want to know?"):
         queries=prompt,
     )
 
-    #fake generator on llm answer (string)
+    # fake generator on llm answer (string)
     def string_generator(strlist):
         """Takes a string to make a generator out of this, allowing to have a nice live token generation effect"""
         for s in strlist:
@@ -45,13 +48,12 @@ if prompt := st.chat_input("What do you want to know?"):
             time.sleep(0.008)
 
     stream = string_generator(response)
-    
 
     st.session_state.messages.append({"role": "user", "content": prompt})
 
-
-    with st.chat_message("assistant"):
-        st.write_stream(stream)
-        #st.markdown(response)   #writes in the chat the llm answer
-    st.session_state.messages.append({"role": "assistant", "content": response}) #updates the chat history once a new prompt is given
-
+    message = st.chat_message("assistant")
+    message.write_stream(stream)
+    # writes in the chat the llm answer in a fancy way (live token generation effect)
+    st.session_state.messages.append(
+        {"role": "assistant", "content": response}
+    )  # updates the chat history once a new prompt is given
