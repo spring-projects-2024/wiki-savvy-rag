b805d8fd10ffaf55f592b7451334baf75479ca61
MattiaSC01 <mattia.scardecchia@studbocconi.it>
2024-05-24 17:57:48 +0200

    feat: load model from checkpoint

--- bash_scripts/run_file.sh
+++ bash_scripts/run_file.sh
@@ -2,14 +2,14 @@
 
 #SBATCH --job-name="run_file"
 #SBATCH --account=3144860
-#SBATCH --partition=debug
+#SBATCH --partition=gpu
 #SBATCH --gpus=1
 #SBATCH --output=/home/3144860/wiki/wiki-savvy-rag/out/%x_%j.out # %x gives job name and %j gives job id
 #SBATCH --error=/home/3144860/wiki/wiki-savvy-rag/err/%x_%j.er
-#SBATCH --nodelist=gnode04
+#SBATCH --nodelist=gnode02
 #SBATCH --cpus-per-task=4
 #SBATCH --mem=8G
-#SBATCH --qos=debug
+#SBATCH --qos=normal
 
 
 cd /home/3144860/wiki/wiki-savvy-rag
@@ -18,6 +18,6 @@ source /home/3144860/miniconda3/bin/activate nlp
 
 conda info --envs
 
-python backend/vector_database/faiss_wrapper.py
+python scripts/load_pretrained.py
 
 conda deactivate
---
+++ scripts/load_pretrained.py
@@ -0,0 +1,44 @@
+import yaml
+import argparse
+from backend.model.rag_handler import RagHandler
+
+
+config_path = "configs/training/final.yaml"
+with open(config_path, "r") as f:
+    config = yaml.safe_load(f)
+
+device = config["device"]
+model_name = config["model_name"]
+run_id = config["run_id"]
+use_qlora = config["use_qlora"]
+optimizer_params = config["optimizer_params"]
+max_epochs = config["max_epochs"]
+batch_size = config["batch_size"]
+gradient_accumulation_steps = config["gradient_accumulation_steps"]
+log_to_wandb = config["log_to_wandb"]
+log_interval = config["log_interval"]
+checkpoint_interval_steps = config["checkpoint_interval_steps"]
+seed = config["seed"]
+wandb_project = config["wandb_project"]
+validation_interval = config["validation_interval"]
+validation_samples = config["validation_samples"]
+watch_model = config["watch_model"]
+
+llm_generation_config = config.get("llm_generation_config", {})
+llm_kwargs = config.get("llm_kwargs", None)
+tokenizer_kwargs = config.get("tokenizer_kwargs", None)
+faiss_kwargs = config.get("faiss_kwargs", None)
+pretrained_model_path = "/home/3144860/wiki/wiki-savvy-rag/checkpoints/final/step100"
+
+rag_handler = RagHandler(
+    model_name=model_name,
+    device=device,
+    use_qlora=use_qlora,
+    llm_generation_config=llm_generation_config,
+    llm_kwargs=llm_kwargs,
+    tokenizer_kwargs=tokenizer_kwargs,
+    faiss_kwargs=faiss_kwargs,
+    pretrained_model_path=pretrained_model_path,
+)
+
+print("Loaded!")
