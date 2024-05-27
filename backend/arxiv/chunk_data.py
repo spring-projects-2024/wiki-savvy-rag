b546c9bd5fbef58cd9ef1f28a63649bc0c2f3244
C4ntor <116038756+C4ntor@users.noreply.github.com>
2024-05-27 18:06:58 +0200

    fix: arxiv chunk size is now smaller

--- backend/arxiv/chunk_data.py
+++ backend/arxiv/chunk_data.py
@@ -5,7 +5,7 @@ import arxiv
 
 client = arxiv.Client(delay_seconds=0.0)
 
-SHORT_CHUNK_LENGTH = 50
+SHORT_CHUNK_LENGTH = 45
 TOO_SHORT_CHUNK_LENGTH = 1000
 
 
@@ -16,6 +16,7 @@ def clean_latex_code(paragraph):
     cl = re.sub("(?:={4,}|.*\n)", "", cl)
     cl = re.sub(r"<cit\.>", "", cl)
     cl = re.sub(r"[\w.-]+\.[\w.-]+\.[\w.-]+", "", cl)
+    cl = re.sub(r"\bbbl\.editors\b|\bbbl\.editor if\b", "", cl)
     return cl
 
 
@@ -23,7 +24,6 @@ def extract_chunks(id):
     """Given the id of a paper, extracts all relevant chunks"""
     doc = get_plain_doc_from_id(id)
     first_step_chunks = []
-    final_chunks = []
     for paragraph in doc.split("\n\n"):
         cleaned_par = clean_latex_code(paragraph)
         nospace = re.sub(r"\s+", "", cleaned_par)
@@ -33,19 +33,7 @@ def extract_chunks(id):
     if len(first_step_chunks) == 0:
         return None
     else:
-        for i in range(len(first_step_chunks)):
-            if (
-                len(first_step_chunks[i]) <= TOO_SHORT_CHUNK_LENGTH
-                and i < len(first_step_chunks) - 1
-            ):
-                first_step_chunks[i + 1] = (
-                    first_step_chunks[i + 1] + "" + first_step_chunks[i]
-                )
-            else:
-                final_chunks.append(first_step_chunks[i])
-            i += 1
-
-        return final_chunks
+        return first_step_chunks
 
 
 def papers_to_chunks(ids: List[str]):
@@ -55,3 +43,19 @@ def papers_to_chunks(ids: List[str]):
         title = get_title_from_id(id, client)
         chunks[title] = ch
     return chunks
+
+
+def split_text_into_paragraphs(text):
+    paragraphs = text.split("\n")
+    return [para.strip() for para in paragraphs if para.strip()]
+
+
+def process_text_list(text_list):
+    processed_list = []
+    for text in text_list:
+        if len(text) > 1000:
+            paragraphs = split_text_into_paragraphs(text)
+            processed_list.extend(paragraphs)
+        else:
+            processed_list.append(text)
+    return processed_list
