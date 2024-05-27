18f946afa53073f0ef917973b2af5ea0049171b4
DarioFi <filatrella.dario@gmail.com>
2024-05-27 22:59:02 +0200

    chore: added pngs

--- scripts/benchmark/analysis.py
+++ scripts/benchmark/analysis.py
@@ -7,7 +7,7 @@ import time
 from typing import List
 import matplotlib.pyplot as plt
 
-TRUST_AFTER = datetime.datetime(year=2024, month=5, day=27, hour=21, minute=43)
+TRUST_AFTER = datetime.datetime(year=2024, month=5, day=27, hour=22, minute=9)
 
 def extract_steps(name):
     m = re.search(r"step(\d+)", name)
@@ -59,11 +59,17 @@ for d in data:
     cfpath = d["args"]["config_path"]
     if d["args"]["k_shot"] not in k_shot_ok:
         continue
+    if d["args"]["inference_type"] != "replug":
+        continue
+
+
     new_data[cfpath] = new_data.get(cfpath, [])
 
     if d["args"]["use_rag"] == False:
         new_data[cfpath].append((0, d["metrics"]["accuracy"]))
         continue
+
+
 
     new_data[cfpath].append((d["args"]["n_docs_retrieved"], d["metrics"]["accuracy"]))
 
@@ -75,60 +81,19 @@ for name, values in new_data.items():
     print(x, y)
     plt.plot(x, y, label=name, marker="^")
 
-plt.title("Accuracy (k_shot=0)")
+plt.title("Replug before finetuning (k_shot=0)")
 plt.xlabel("n_docs_retrieved")
 plt.ylabel("accuracy")
 plt.grid()
-plt.legend()
-plt.show()
-
-# - accuracy vs number of documents retrieved, including 0 (can be done for a few chkpts, putting the curves in the same graph, as above. i would not use too many chkpts though)
-
-# - accuracy vs training_step (using replug, kshot 0, fixed number of documents (e.g. 3))
+# save to disk
+plt.savefig("../../media/replug_before_finetuning.png")
 
 
-n_docs_retrieved = 1
-k_shot = 0
-inference_type = "replug"
-
-new_data = {}  # in the form x: y
-for d in data:
-    if d["args"]["n_docs_retrieved"] != n_docs_retrieved:
-        continue
-    if d["args"]["k_shot"] != k_shot:
-        continue
-    if d["args"]["inference_type"] != inference_type:
-        continue
-    if d["args"]["use_rag"] == False:
-        continue
-
-    cfpath = d["args"]["config_path"]
-
-    # cfpath is like "configs/checkpoints/step1000.yaml"
-    # we want to extract the step number
-
-    step = extract_steps(cfpath)
-
-    new_data[step] = d["metrics"]["accuracy"]
-
-print(new_data)
-x, y = zip(*sorted(new_data.items()))
-
-plt.plot(x, y, marker="^")
-
-plt.title(
-    f"Accuracy (k_shot={k_shot}, n_docs_retrieved={n_docs_retrieved}, inference_type={inference_type})"
-)
-plt.xlabel("training_step")
-plt.ylabel("accuracy")
-plt.grid()
-plt.show()
 
 
 # accuracy vs kshots (using replug, fixed number of documents (e.g. 3), fixed training_step)
 
 n_docs_retrieved = 3
-config_path = "configs/checkpoints/step1000.yaml"
 config_path = "configs/llm_vm.yaml"
 inference_type = "replug"
 
@@ -153,9 +118,23 @@ print(new_data)
 x, y = zip(*sorted(new_data.items()))
 plt.plot(x, y, marker="^")
 plt.title(
-    f"Accuracy (config_path={config_path}, n_docs_retrieved={n_docs_retrieved}, inference_type={inference_type})"
+    # f"Baseline LLM with {n_docs_retrieved} documents and {inference_type}"
+    "Rag before finetuning"
 )
 plt.xlabel("k_shot")
 plt.ylabel("accuracy")
 plt.grid()
-plt.show()
+plt.savefig("../../media/k_shot_ablation.png")
+
+
+# find naive accuracy on config/llm_vm.yaml
+
+for d in data:
+    if d["args"]["config_path"] == "configs/llm_vm.yaml" and d["args"]["k_shot"] == 0 and \
+        d["args"]["n_docs_retrieved"] == 3 and d["args"]["inference_type"] == "replug":
+            print("replug:  ", d["metrics"]["accuracy"])
+
+    if d["args"]["config_path"] == "configs/llm_vm.yaml" and d["args"]["k_shot"] == 0 and \
+        d["args"]["n_docs_retrieved"] == 3 and d["args"]["inference_type"] == "naive":
+            print("naive:   ", d["metrics"]["accuracy"])
+
