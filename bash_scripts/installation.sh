09d5623b4c3a37ad164f0780776187c6681854d7
MattiaSC01 <mattia.scardecchia@studbocconi.it>
2024-10-18 23:56:39 +0200

    enable installing only inference dependencies in installation script

--- bash_scripts/installation.sh
+++ bash_scripts/installation.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
+echo "Installing project dependencies..."
 pip install -r requirements.txt
+
+echo "Installing project..."
 pip install -e .
-git clone https://github.com/MattiaSC01/JEPA.git
-cd JEPA
-pip install -r requirements.txt
-pip install -e .
+
+if [[ " $@ " =~ " --finetuning-dependencies " ]]; then
+    echo "Installing finetuning dependencies..."
+    git clone https://github.com/MattiaSC01/JEPA.git
+    cd JEPA
+    pip install -r requirements.txt
+    pip install -e .
+else
+    echo "Skipping finetuning dependencies installation."
+fi
